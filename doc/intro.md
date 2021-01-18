a6d93719874c3b81783bbfdda085a03caf44d9c1
Roelof Wobben <r.wobben@home.nl>
2021-01-18 18:59:26 +0100

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to paintings
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/paintings/core.clj
@@ -0,0 +1,54 @@
+(ns paintings.core
+  (:require [cheshire.core :as json]
+            [clj-http.client :as client]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.adapter.jetty :as ring.adapter.jetty]))
+
+
+(defn image-url-size [image]
+  (let [data (select-keys image [:width :height])
+        url (get-in image [:tiles 0 :url])]
+    (assoc data :url url)))
+
+(defn take-image-data [image-data object-number]
+  (->> image-data
+       (sort-by :name)
+       (last)
+       (image-url-size)
+       (merge {:object-number object-number})))
+
+(defn assoc-image [object-number]
+  (-> (client/get (str "https://www.rijksmuseum.nl/api/nl/collection/" object-number "/tiles")
+                  {:query-params {:key "14OGzuak"
+                                  :format "json"}})
+      (:body)
+      (json/parse-string true)
+      (:levels)
+      (take-image-data object-number)))
+
+(defn take-data [api-data]
+  (->> api-data
+       (map :objectNumber)
+       (map assoc-image)))
+
+(defn display-data []
+  (-> (client/get "https://www.rijksmuseum.nl/api/nl/collection"
+                  {:query-params {:key "14OGzuak"
+                                  :format "json"
+                                  :type "schilderij"
+                                  :toppieces "True"}})
+      :body
+      (json/parse-string true)
+      :artObjects
+      (take-data)))
+
+(defroutes app
+  (GET "/" [] (display-data)))
+
+
+(ring.adapter.jetty/run-jetty
+ app {:port  8080, :join? false})
+
+
+
