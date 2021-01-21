452e21965b83c9841489121deb6189f70d6cede2
Roelof Wobben <r.wobben@home.nl>
2021-01-21 10:51:00 +0100

    almost ready

--- src/paintings/display.clj
+++ src/paintings/display.clj
@@ -14,6 +14,36 @@
    [:dic {:class "face left"}]
    [:div {:class= "face bottom"}]])
 
+
+(defn display-middle1 [images]
+  [:div {:class "shelf middle1"}
+   [:div {:class "face top"}]
+   [:div {:class "face front"}
+    (second images)]
+   [:div {:class "face back"}]
+   [:dic {:class "face left"}]
+   [:div {:class= "face bottom"}]])
+
+(defn display-middle2 [images]
+  [:div {:class "shelf middle1"}
+   [:div {:class "face top"}]
+   [:div {:class "face front"}
+    (nth images 3)]
+   [:div {:class "face back"}]
+   [:dic {:class "face left"}]
+   [:div {:class= "face bottom"}]])
+
+
+(defn display-bottom [images]
+  [:div {:class "shelf middle1"}
+   [:div {:class "face top"}]
+   [:div {:class "face front"}
+    (nth images 4)]
+   [:div {:class "face back"}]
+   [:dic {:class "face left"}]
+   [:div {:class= "face bottom"}]])
+
+
 (defn display-body [data]
   (let [images  (partition 3 (map create-image-element data))]
     [:body
@@ -21,7 +51,10 @@
       [:div {:class "roll-camera"}
        [:div {:class "move-camera"}
         [:div {:class "wallpaper"}]
-        (display-top-shelf images)]]]]))
+        (display-top-shelf images)
+        (display-middle1 images)
+        (display-middle2 images)
+        (display-bottom images)]]]]))
 
 (defn convert-to-hiccup [data]
   [:html
